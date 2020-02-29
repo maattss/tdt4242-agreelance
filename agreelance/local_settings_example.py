4452cd6365dbcc033d989289967e7cb7f219e528
Mats Tyldum <ma.tyldum@gmail.com>
2020-02-29 13:04:50 +0100

    Update email settings

--- agreelance/local_settings_example.py
+++
@@ -1,8 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-# Turn on less secure apps to make it work
-# https://support.google.com/accounts/answer/6010255?hl=en
-EMAIL_HOST_USER = '<mail>@gmail.com'
-EMAIL_HOST_PASSWORD = '<password>'
