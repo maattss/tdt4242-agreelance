c08af24432bc1c734a3bc6942acf46a6f65ac337
Mats Tyldum <ma.tyldum@gmail.com>
2020-03-17 13:34:22 +0100

    Extended boundaries for give project offer test

--- projects/forms.py
+++ projects/forms.py
@@ -28,7 +28,7 @@ class ProjectStatusForm(forms.ModelForm):
 class TaskOfferForm(forms.ModelForm):
     title = forms.CharField(max_length=200)
     description = forms.Textarea()
-    price = forms.NumberInput()
+    price = forms.IntegerField(min_value=1)
 
     class Meta:
         model = TaskOffer
