99bd78e713397c6a96c75768872eaa793ec7b526
Mats Tyldum <ma.tyldum@gmail.com>
2020-03-18 09:25:20 +0100

    Added 2-way domain test

--- user/tests.py
+++ user/tests.py
@@ -162,8 +162,9 @@ class TestSignupPageBoundary(TestCase):
         form = SignUpForm(data)
         self.assertFalse(form.is_valid())
 
-# 2-way domain tests of the sign-up page
-class TestSignupPageDomain(TestCase):
+"""
+# 2-way domain tests of the sign-up page - DEPRECATED
+class TestSignupPageDomainDeprecated(TestCase):
     def setUp(self):
         fake = Faker() # Generate fake data using a faker generator
 
@@ -244,9 +245,10 @@ class TestSignupPageDomain(TestCase):
         }
         form = SignUpForm(data)
         self.assertTrue(form.is_valid())
+"""
 
-# 2-way domain tests of the sign-up page **USING ALLPAIRSPY**
-class TestSignupPageDomain2(TestCase):
+# 2-way domain tests of the sign-up page
+class TestSignupPageDomain(TestCase):
     def setUp(self):
         fake = Faker() # Generate fake data using a faker generator
 
@@ -259,13 +261,16 @@ class TestSignupPageDomain2(TestCase):
         self.approved_password_2 = fake.password()
         self.approved_categories = [ProjectCategory.objects.create(pk=1)]
         
-        # Configure parameters for use in the test cases
+        # Configure declined parameters for use in the test cases
         username_1 = ""
         username_2 = FuzzyText(length=50, chars=string.punctuation).fuzz() # Special charaters
         username_3 = FuzzyText(length=151).fuzz()
 
-        textfield_1 = ""
-        textfield_2 = FuzzyText(length=100).fuzz()
+        textfield_30_1 = ""
+        textfield_30_2 = FuzzyText(length=40).fuzz()
+
+        textfield_50_1 = ""
+        textfield_50_2 = FuzzyText(length=60).fuzz()
     
         email_1 = FuzzyText(length=245, suffix="@gmail.com").fuzz()
         email_2 = FuzzyText(length=20).fuzz()
@@ -278,40 +283,50 @@ class TestSignupPageDomain2(TestCase):
 
         declined_parameters = [
             [username_1, username_2, username_3],
-            [textfield_1, textfield_2],
+            [textfield_30_1, textfield_30_2],
+            [textfield_50_1, textfield_50_2],
             [email_1, email_2],
             [email_1, email_2],
             [password_1, password_2, password_3],
             [password_1, password_2, password_3],
             [categories]
         ]
+
         # Generate all possible combinations
         self.declined_combinations = list(AllPairs(declined_parameters))
 
-        # TODO: Remove if not used
         self.declined_parameters_dict = OrderedDict({
             "username": [username_1, username_2, username_3],
-            "textfield": [textfield_1, textfield_2],
+            "textfield_30": [textfield_30_1, textfield_30_2],
+            "textfield_50": [textfield_50_1, textfield_50_2],
             "email": [email_1, email_2],
             "password": [password_1, password_2, password_3],
             "categories": [categories]
         })
 
-        self.declined_combinations_dict = AllPairs(self.declined_parameters_dict)
-
     # All combinations
     def test_declined_combinations(self):
-        print("Test all combinations:\n")
-        # Loop trough combinations
-        for index, pairs in enumerate(AllPairs(self.declined_parameters_dict)):
-            # Username test
-            # Textfield test
-            # Email test
-            # Password test
-            # Categories test
-            print("{:2d}: {}".format(index, pairs.username))
-        
-        # AssertFalse for all combos
+        for _, pairs in enumerate(AllPairs(self.declined_parameters_dict)):
+            data = {
+                'username': pairs.username,
+                'first_name': pairs.textfield_30,
+                'last_name': pairs.textfield_30,
+                'categories': pairs.categories,
+                'company': pairs.textfield_30,
+                'email': pairs.email,
+                'email_confirmation': pairs.email,
+                'password1': pairs.password,
+                'password2': pairs.password,
+                'phone_number': pairs.textfield_50,
+                'country': pairs.textfield_50,
+                'state': pairs.textfield_50,
+                'city': pairs.textfield_50,
+                'postal_code': pairs.textfield_50,
+                'street_address': pairs.textfield_50,
+            }
+
+            form = SignUpForm(data)
+            self.assertFalse(form.is_valid())
 
     # Special test case when emails are not equal
     def test_different_email(self):
